22f5f5705c4002b24284a73bea02927dc867afd9
Sekator778 <sekator778@gmail.com>
2020-02-14 16:21:30 +0200

    5.6. Простые числа.[#237918]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,9 +3,11 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean result = false;
-        for (int i = 2; i < number - 1; i++) {
+        if (number == 2) return true;
+        for (int i = 2; i <= number - 1; i++) {
             if ((number % i) == 0) {
                 result = false;
+                break;
             } else result = true;
         }return result;
     }
--- chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -23,4 +23,9 @@ public class CheckPrimeNumberTest {
         boolean rsl = CheckPrimeNumber.check(1);
         assertThat(rsl, is(false));
     }
+    @Test
+    public void when2() {
+        boolean rsl = CheckPrimeNumber.check(2);
+        assertThat(rsl, is(true));
+    }
 }

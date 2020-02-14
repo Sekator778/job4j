f675281e420db4f8726c2278358255061676ec41
Sekator778 <sekator778@gmail.com>
2020-02-14 18:21:57 +0200

    6.1. Классический поиск перебором.[#237847]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public int indexOf(int[] data, int el) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayNotHaveElement() {
+        assertThat(new FindLoop().indexOf(new int[] {1, 2, 3, 4, -10, 0}, 7), is(-1));
+    }
+}

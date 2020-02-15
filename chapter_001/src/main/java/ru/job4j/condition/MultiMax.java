758ca41b748a49b64350e76b179458ce151e65ae
Sekator778 <sekator778@gmail.com>
2020-02-15 11:32:00 +0200

    Задачи от Операции сравнения[#237839]

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -2,10 +2,6 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public int max(int first, int second, int third) {
-        if (first > second && first > third){
-            return first;
-        }else if (second > first && second > third){
-            return second;
-        }else return third;
+        return (first > second) ? ((first > third) ? first : second) : ((second > third) ? second : third);
     }
 }

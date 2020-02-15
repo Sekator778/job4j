c492c5bf4666e3527eadf76550aec0b3a35986e1
Sekator778 <sekator778@gmail.com>
2020-02-15 21:48:39 +0200

    6.8. Дефрагментация массива.[#237868]

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index;
+               while (point < array.length - 1) {
+                   String temp = array[point + 1];
+                   array[point + 1] = array[point];
+                   array[point] = temp;
+                   point++;
+               }
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}

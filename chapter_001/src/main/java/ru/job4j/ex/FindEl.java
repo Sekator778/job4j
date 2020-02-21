38a851c2115fabb8b192239ab4477312260344b4
Sekator778 <sekator778@gmail.com>
2020-02-21 12:12:36 +0200

    0.4. Пользовательские исключения.[#238005]

--- chapter_001/src/main/java/ru/job4j/ex/FindEl.java
+++ chapter_001/src/main/java/ru/job4j/ex/FindEl.java
@@ -2,16 +2,14 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        if (key == null) {
-            throw new ElementNotFoundException("key null");
-        }
         int rsl = -1;
-        for (String s : value) {
-            if (s.equals(key)) {
-                rsl = 1;
-            } else {
-                rsl = 0;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                rsl = index;
             }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("key null");
         }
         return rsl;
     }

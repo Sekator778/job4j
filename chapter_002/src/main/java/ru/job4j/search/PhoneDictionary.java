767b561434ed918a1ecc189087f9726067681c43
Sekator778 <sekator778@gmail.com>
2020-02-22 16:12:53 +0200

    1. Телефонный справочник на базе ArrayList[#237950]

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -16,7 +16,7 @@ public class PhoneDictionary {
      */
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        for (Person person : result
+        for (Person person : persons
         ) {
             if (person.getAddress().contains(key)
                     || person.getName().contains(key)

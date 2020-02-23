d96b676cb75e1aa71973e055aa70c7eba8320caf
Sekator778 <sekator778@gmail.com>
2020-02-23 21:38:25 +0200

    2. Идентичные тексты.[#237964]

--- chapter_002/src/main/java/ru/job4j/collection/UniqueText.java
+++ chapter_002/src/main/java/ru/job4j/collection/UniqueText.java
@@ -5,14 +5,15 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-        boolean rsl = false;
+        boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
         Collections.addAll(check, origin);
         for (String string : text) {
-            if (check.contains(string)) {
-                rsl = true;
+            if (!check.contains(string)) {
+                rsl = false;
+                break;
             }
         }
         return rsl;

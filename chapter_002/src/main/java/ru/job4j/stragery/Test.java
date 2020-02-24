b341a0e6521fb07bb36c900ac4322d958ea4b5a7
Sekator778 <sekator778@gmail.com>
2020-02-24 20:09:09 +0200

    comparator

---
+++ chapter_002/src/main/java/ru/job4j/stragery/Test.java
@@ -0,0 +1,12 @@
+package ru.job4j.stragery;
+
+public class Test {
+    public static void main(String[] args) {
+//        Integer a = null;
+//        if (a != null & a.toString().equals("1")) {
+//            System.out.println("1");
+//        } else {
+//            System.out.println("a is null");
+//        }
+    }
+}

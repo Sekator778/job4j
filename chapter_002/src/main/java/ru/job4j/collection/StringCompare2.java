61bc2053c5c8ba76155aeac96866ae6f99f18e7e
Sekator778 <sekator778@gmail.com>
2020-02-25 20:42:23 +0200

    compare StringCompare2

---
+++ chapter_002/src/main/java/ru/job4j/collection/StringCompare2.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare2 implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        char[] value = left.toCharArray();
+        char[] other = right.toCharArray();
+        int len1 = value.length;
+        int len2 = other.length;
+        int lim = Math.min(len1, len2);
+        for (int i = 0; i < lim; i++) {
+            if (value[i] != other[i]) {
+                result = Character.compare(value[i], other[i]);
+            } else {
+                result = len1 - len2;
+            }
+        }
+        return result;
+    }
+}

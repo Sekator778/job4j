4371591f42381eab9b06946dfeae0812172cd4eb
Sekator778 <sekator778@gmail.com>
2020-02-27 09:19:54 +0200

    департамент 1

---
+++ chapter_002/src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,51 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    /**
+     * Добавляем недостающие департаменты.
+     *
+     * @param deps Множество департаментов.
+     */
+    public static List<String> fillGaps(List<String> deps) {
+        HashSet<String> tmp = new LinkedHashSet<>();
+        HashSet<String> rsl = new HashSet<String>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                tmp.add(el + "/");
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String s : tmp
+        ) {
+            sb.append(s);
+            rsl.add(sb.toString());
+        }
+        return new ArrayList<>(rsl);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+    }
+
+    public static void sortDesc(List<String> orgs) {
+    }
+
+    public static void main(String[] args) {
+        List<String> input = new ArrayList<>();
+        input.add("K1/SK1");
+        input.add("SK1/SSK1");
+        input.add("K1/SK1/SSK2");
+//        input.add("K2");
+//        input.add("K2/SK1/SSK1");
+//        input.add("K2/SK1/SSK2");
+        List<String> result = Departments.fillGaps(input);
+        for (String s : result
+        ) {
+            System.out.println(s);
+        }
+
+
+    }
+}

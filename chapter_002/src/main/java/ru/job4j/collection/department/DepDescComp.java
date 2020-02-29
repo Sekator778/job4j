72723dabfdfb32017dd37c213a91f05939268caa
Sekator778 <sekator778@gmail.com>
2020-02-29 16:17:40 +0200

    departament

--- chapter_002/src/main/java/ru/job4j/collection/department/DepDescComp.java
+++ chapter_002/src/main/java/ru/job4j/collection/department/DepDescComp.java
@@ -6,12 +6,12 @@ public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
         int result = 0;
-        if (o1.length() == o2.length()
-                || o1.length() < o2.length() && !o1.equals(o2.substring(0, o1.length()))) {
+        int len1 = o1.length();
+        int len2 = o2.length();
+        boolean equal = o1.equals(o2.substring(0, len1));
+        if ((len1 == len2) || (len1 < len2) && (!equal)) {
             result = o2.compareTo(o1);
-        } else
-        if (o1.length() < o2.length() && o1.equals(o2.substring(0, o1.length()))
-                || o1.length() > o2.length() && o2.equals(o1.substring(0, o2.length()))) {
+        } else if ((len1 < len2) && (equal) || len1 > len2 && o2.equals(o1.substring(0, len2))) {
             result = o1.compareTo(o2);
         }
         return result;

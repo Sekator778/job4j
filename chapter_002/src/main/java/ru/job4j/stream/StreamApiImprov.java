d3472442fd200fcbaf4006910f6d7ec831b31570
Sekator778 <sekator778@gmail.com>
2020-03-02 17:32:30 +0200

    ?  0. Stream API улучшения[#237816]

--- chapter_002/src/main/java/ru/job4j/stream/StreamApiImprov.java
+++ chapter_002/src/main/java/ru/job4j/stream/StreamApiImprov.java
@@ -21,29 +21,29 @@ import java.util.stream.Stream;
 public class StreamApiImprov {
     public static void main(String[] args) {
         List.of(2, 4, 3, 4).stream()
-                .takeWhile(v -> v % 2 == 0)
+                .takeWhile(v -> v < 4)
                 .map(v -> String.format(" %s", v))
                 .forEach(System.out::print);
-        System.out.println();
-        System.out.println("=======================");
-
-        List.of(2, 4, 3, 4).stream()
-                .dropWhile(v -> v % 2 == 0)
-                .map(v -> String.format(" %s", v))
-                .forEach(System.out::print);
-        System.out.println();
-        System.out.println("=======================");
-
-        Stream.of(1, null, 2, null, 3)
-                .flatMap(Stream::ofNullable)
-                .map(v -> String.format(" %s", v))
-                .forEach(System.out::print);
-        System.out.println();
-        System.out.println("=======================");
-
-        Stream.iterate(0, i -> i < 10, i -> i + 1)
-                .map(v -> String.format(" %s", v))
-                .forEach(System.out::print);
+//        System.out.println();
+//        System.out.println("=======================");
+//
+//        List.of(2, 4, 3, 4).stream()
+//                .dropWhile(v -> v % 2 == 0)
+//                .map(v -> String.format(" %s", v))
+//                .forEach(System.out::print);
+//        System.out.println();
+//        System.out.println("=======================");
+//
+//        Stream.of(1, null, 2, null, 3)
+//                .flatMap(Stream::ofNullable)
+//                .map(v -> String.format(" %s", v))
+//                .forEach(System.out::print);
+//        System.out.println();
+//        System.out.println("=======================");
+//
+//        Stream.iterate(0, i -> i < 10, i -> i + 1)
+//                .map(v -> String.format(" %s", v))
+//                .forEach(System.out::print);
     }
 
 }
--- chapter_002/src/main/java/ru/job4j/stream/Student.java
+++ chapter_002/src/main/java/ru/job4j/stream/Student.java
@@ -1,11 +1,12 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class Student {
+public class Student implements Comparable<Student> {
     private int score;
     private String surname;
 
@@ -20,11 +21,24 @@ public class Student {
 
     @Override
     public String toString() {
-        return String.valueOf(score);
+        return surname + " : " + score;
     }
 
+    @Override
+    public int compareTo(Student o) {
+        return Integer.compare(this.score, o.score);
+    }
+
+
     public static List<Student> createStudentList() {
-        return new ArrayList<>();
+        Student s1 = new Student("Bob", 12);
+        Student s2 = new Student("Piter", 5);
+        Student s3 = new Student("Voron", 32);
+        Student s4 = new Student("Kot", 65);
+        Student s5 = new Student(null, 34);
+        List<Student> list = List.of(s1, s2, s3, s4, s5);
+
+        return list;
     }
 
     public static Map<String, Student> createStudentMap(List<Student> studentList) {
@@ -32,5 +46,20 @@ public class Student {
                 i -> i.surname,
                 i -> i
         ));
+    }
+
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        Collections.sort(students);
+
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .takeWhile(s -> (s.score > bound))
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<Student> studentList = createStudentList();
+        studentList = levelOf(studentList, 15);
+        studentList.forEach(System.out::println);
     }
 }

5c34cb20c0f0505e83e837b22e4aa4db556b24bc
Sekator778 <sekator778@gmail.com>
2020-03-02 12:29:18 +0200

    student list.Collectors.toMap

--- chapter_002/src/main/java/ru/job4j/stream/Student.java
+++ chapter_002/src/main/java/ru/job4j/stream/Student.java
@@ -1,18 +1,36 @@
 package ru.job4j.stream;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Student {
     private int score;
+    private String surname;
 
     public int getScore() {
         return score;
     }
 
-    public Student(int score) {
+    public Student(String surname, int score) {
+        this.surname = surname;
         this.score = score;
     }
 
     @Override
     public String toString() {
         return String.valueOf(score);
+    }
+
+    public static List<Student> createStudentList() {
+        return new ArrayList<>();
+    }
+
+    public static Map<String, Student> createStudentMap(List<Student> studentList) {
+        return studentList.stream().distinct().collect(Collectors.toMap(
+                i -> i.surname,
+                i -> i
+        ));
     }
 }

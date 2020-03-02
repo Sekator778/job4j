92651b4bd5e388f76de1a4f3af5e24c8ae7b4750
Sekator778 <sekator778@gmail.com>
2020-03-02 13:11:06 +0200

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API.[#237808]

--- chapter_002/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -8,10 +8,21 @@ import java.util.Map;
 public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Добавляет нового пользователя в коллекцию пользователей и счетов.
+     *
+     * @param user добавляемый пользователь.
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<Account>());
     }
 
+    /**
+     * Добавляет новый счет пользователю
+     *
+     * @param passport id пользователя
+     * @param account  acc для добавления
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -23,13 +34,8 @@ public class BankService {
     }
 
     public User findByPassport(String passport) {
-        for (User user : users.keySet()
-        ) {
-            if (user.getPassport().equals(passport)) {
-                return user;
-            }
-        }
-        return null;
+        return users.keySet().stream().filter(
+                user -> user.getPassport().equals(passport)).findFirst().orElse(null);
     }
 
     public Account findByRequisite(String passport, String requisite) {
@@ -38,13 +44,8 @@ public class BankService {
             return null;
         }
         List<Account> list = users.get(user);
-        for (Account acc : list
-        ) {
-            if (acc.getRequisite().equals(requisite)) {
-                return acc;
-            }
-        }
-        return null;
+        return list.stream().filter(
+                account -> account.getRequisite().equals(requisite)).findFirst().orElse(null);
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,

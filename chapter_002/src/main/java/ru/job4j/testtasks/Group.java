6ff5a4ca3ae4d1f74097f698621c33a89df7f52d
Sekator778 <sekator778@gmail.com>
2020-03-07 13:04:32 +0200

    собес 4.1.

---
+++ chapter_002/src/main/java/ru/job4j/testtasks/Group.java
@@ -0,0 +1,21 @@
+package ru.job4j.testtasks;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Group {
+    public static Map<String, Set<String>> sections(List<Student> students) {
+        return null;
+    }
+    static class Holder {
+        String key, value;
+
+        Holder(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
+
+

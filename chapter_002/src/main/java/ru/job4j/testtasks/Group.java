cca60a57ef441f3d61eac5ffc3ffd628f6158ff8
Sekator778 <sekator778@gmail.com>
2020-03-08 22:33:21 +0200

    стрим + лямбда тест

--- chapter_002/src/main/java/ru/job4j/testtasks/Group.java
+++ chapter_002/src/main/java/ru/job4j/testtasks/Group.java
@@ -1,21 +1,38 @@
 package ru.job4j.testtasks;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Group {
     public static Map<String, Set<String>> sections(List<Student> students) {
-        return null;
+        return students.stream()
+                .flatMap(student -> Stream.of(student)
+                .collect(  // собираем объект Holder с unit и name// собираем карту
+                Collectors.groupingBy(student::getName) // определяем группировку
+                        Collector.of(
+                                HashSet::new, // аккумулятор.
+                                (set, el) -> // как добавлять данные.
+                                        (left, right) -> { left.addAll(right); return left; } // для агрегации.
+                        )
+                )
+        );
     }
     static class Holder {
         String key, value;
-
         Holder(String key, String value) {
             this.key = key;
             this.value = value;
         }
     }
 }
-
-
+//public Set<String> ndLongTracks(List<Album> albums) {
+//return albums.stream()
+//. flatMap(album -> album.getTracks())
+//. filter(track -> track.getLength() > 60)
+//.map(track -> track.getName())
+//.collect(toSet());

ae4d983e66b779ddd96a355a0f8186b49d179ae9
Sekator778 <sekator778@gmail.com>
2020-05-24 10:44:58 +0300

    check

--- chapter_002/src/test/java/ru/job4j/pseudo/SquareTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/SquareTest.java
@@ -7,11 +7,6 @@ import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-/**
- * @author Petr Arsentev (parsentev@yandex.ru)
- * @version $Id$
- * @since 0.1
- */
 public class SquareTest {
     @Test
     public void whenDrawSquare() {

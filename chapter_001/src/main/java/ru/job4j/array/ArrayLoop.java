ab78480ff2b443895adb1b026326bd8608178f30
Sekator778 <sekator778@gmail.com>
2021-07-21 17:11:35 +0300

    check

--- chapter_001/src/main/java/ru/job4j/array/ArrayLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayLoop.java
@@ -6,8 +6,7 @@ public class ArrayLoop {
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i * 2 + 3;
         }
-        for (int num :
-                numbers) {
+        for (int num : numbers) {
             System.out.println(num);
         }
     }
--- chapter_001/src/main/java/ru/job4j/array/MatrixSum.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixSum.java
@@ -3,10 +3,8 @@ package ru.job4j.array;
 public class MatrixSum {
     public static int sum(int[][] array) {
         int rsl = 0;
-        for (int[] arr :
-                array) {
-            for (int i :
-                    arr) {
+        for (int[] arr : array) {
+            for (int i : arr) {
                 rsl += i;
             }
         }
